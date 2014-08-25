826f78985982d1c888e447b41bc689e4f2e448e3
Peter Neely <pgneely@gmail.com>
2014-08-25 17:46:06 +0100

    Completed search for clients

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -3,31 +3,41 @@
 (function () {
 
   var settingsController = ['User', 'Data', function (User, Data) {
-      var self = this;
+    var self = this;
 
+    self.newClient = null;
+
+    self.search = { name: '' };
+
+    User.whenLoggedIn(function () {
+      self.clients = Data.all('clients');
+      self.show = true;
+    });
+
+    self.add = function () {
+      Data.add(self.newClient).then(resetClient);
+    };
+
+    self.remove = function (client) {
+      Data.remove(client);
+    };
+
+    self.update = function (client) {
+      Data.update(client);
+    };
+
+    self.searching = function () {
+      return self.search.name.length > 0;
+    };
+
+    self.clearSearch = function () {
+      self.search.name = '';
+    };
+
+    function resetClient() {
       self.newClient = null;
-
-      User.whenLoggedIn(function () {
-        self.clients = Data.all('clients');
-        self.show = true;
-      });
-
-      self.add = function () {
-        Data.add(self.newClient).then(resetClient);
-      };
-
-      self.remove = function (client) {
-        Data.remove(client);
-      };
-
-      self.update = function (client) {
-        Data.update(client);
-      };
-
-      function resetClient() {
-        self.newClient = null;
-      }
-    }];
+    }
+  }];
 
   angular
     .module('scrummyApp')
