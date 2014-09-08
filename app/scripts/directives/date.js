a61340c887e0d91b51e24971abcf23225b080571
Peter Neely <pgneely@gmail.com>
2014-09-08 15:05:23 +0100

    Added date picker to time entry popup

---
+++ app/scripts/directives/date.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+
+  angular
+    .module('scrummyApp')
+    .directive('date', DateDirective);
+
+  function DateDirective() {
+    return {
+      templateUrl: 'views/directives/date.html',
+      scope: {
+        scModel: '='
+      },
+      controller: DateController,
+      replace: true
+    };
+  }
+
+  function DateController($scope) {
+
+    $scope.dateOptions = options();
+    $scope.scModel = Date.now();
+    $scope.open = onOpen;
+    $scope.opened = false;    
+
+    function onOpen($event){
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.opened = true;
+      console.log($scope.opened);
+    }
+
+    function options(){
+      return {
+        startingDay: 1,
+        showWeeks: false
+      };
+    }
+  }
+
+})();
