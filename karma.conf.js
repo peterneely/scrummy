fe007e9cd6010b661b42917cf38073c4502808bb
Peter Neely <pgneely@gmail.com>
2014-10-14 16:15:35 +0100

    Getting test framework set up

--- karma.conf.js
+++ karma.conf.js
@@ -12,10 +12,16 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'src/bower_components/angular/angular.js',
-      'src/bower_components/angular/angular-mocks.js',
-      'src/app/scripts/*.js',
-      'src/app/scripts/**/*.js',
-      'test/mock/**/*.js',
+      'src/bower_components/angular-mocks/angular-mocks.js',
+      'src/bower_components/angular-animate/angular-animate.js',
+      'src/bower_components/angular-cookies/angular-cookies.js',
+      'src/bower_components/angular-resource/angular-resource.js',
+      'src/bower_components/angular-route/angular-route.js',
+      'src/bower_components/angular-sanitize/angular-sanitize.js',
+      'src/bower_components/angular-touch/angular-touch.js',
+//      'src/app/*.js',
+      'src/app/**/*.js',
+//      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
@@ -31,7 +37,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
@@ -42,11 +48,31 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: [
+      'PhantomJS'
+    ],
 
+    // Which plugins to enable
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-jasmine'
+    ],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
   });
 };
